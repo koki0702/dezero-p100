4139af610959a3fd93bf6eb9a89f7205832ce8a0
koki0702 <koki0702@gmail.com>
2022-05-28 08:38:25 +0900

    Update

---
+++ precache-manifest.c524df55b8e403d218c598f49bba9a96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "59ddfb063473ae50c5c0",
+    "url": "/dezero-p100/static/css/main.527fa57f.chunk.css"
+  },
+  {
+    "revision": "59ddfb063473ae50c5c0",
+    "url": "/dezero-p100/static/js/main.59ddfb06.chunk.js"
+  },
+  {
+    "revision": "1b1683c089853a555695",
+    "url": "/dezero-p100/static/js/1.1b1683c0.chunk.js"
+  },
+  {
+    "revision": "1562137e007dea2a7c62",
+    "url": "/dezero-p100/static/js/runtime~main.1562137e.js"
+  },
+  {
+    "revision": "4518cb75c46b6633c2ad3d3703831ebb",
+    "url": "/dezero-p100/static/media/cover.4518cb75.jpg"
+  },
+  {
+    "revision": "fc452a02149dd426417411c486022a22",
+    "url": "/dezero-p100/static/media/c1_1.fc452a02.png"
+  },
+  {
+    "revision": "f032ef1af60253f830c25e3daad05581",
+    "url": "/dezero-p100/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dezero-p100/precache-manifest.3b6fc51f9b08dadf1a63724a382fd91c.js"
+  "/dezero-p100/precache-manifest.c524df55b8e403d218c598f49bba9a96.js"
 );
 
 workbox.clientsClaim();
